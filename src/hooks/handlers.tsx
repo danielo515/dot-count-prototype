a4e6aedece315c9e34772e1e360af8b42bee54d2
Danielo Rodriguez <rdanielo@gmail.com>
2020-07-24 20:30:16 +0200

    fix: max/min brush size

--- src/hooks/handlers.tsx
+++ src/hooks/handlers.tsx
@@ -1,11 +1,13 @@
 import { BrushStyle } from "../BrushSelector/BrushSelector";
 import { State } from "./Provider";
+const maxSize = 15;
+const minSize = 1;
 export const handlers = {
   incBrush(state: State, { by = 1 }: { by: number }) {
-    return { ...state, size: state.size + by };
+    return { ...state, size: Math.min(state.size + by, maxSize) };
   },
   decBrush(state: State, { by = 1 }: { by: number }) {
-    return { ...state, size: state.size - by };
+    return { ...state, size: Math.min(state.size - by, minSize) };
   },
   setColor(state: State, { color }: { color: string }) {
     return { ...state, color };
