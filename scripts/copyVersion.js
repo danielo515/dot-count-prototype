ef5ce7ab6c079f3463c7a4af32d0fcd99e1a4a05
Danielo Rodriguez <rdanielo@gmail.com>
2020-07-24 21:10:34 +0200

    more sync

--- scripts/copyVersion.js
+++ scripts/copyVersion.js
@@ -4,6 +4,8 @@ const { promisify } = require("util");
 const writeFile = promisify(_writeFile);
 const readFile = promisify(_readFile);
 const configPath = path.join(__dirname, "../itemcounter/config.xml");
+const cordovaPath = path.join(__dirname, "../itemcounter/package.json");
+const cordovaPkg = require(cordovaPath);
 
 const version = require("../package.json").version;
 async function main() {
@@ -12,8 +14,11 @@ async function main() {
     /version="(\d+\.?){3}"/,
     `version="${version}"`
   );
+  cordovaPkg.version = version;
   await writeFile(configPath, updatedContent, "utf8");
   console.log("wrote", version, "to", configPath);
+  await writeFile(cordovaPath, JSON.stringify(cordovaPkg), "utf8");
+  console.log("wrote", version, "to", cordovaPath);
 }
 
 main().catch(console.error);
